78a76003168fa14d295b52f9f83ba118ae5fea76
Vishal-m <vishalmahesh.study@gmail.com>
2023-12-13 01:19:18 +0530

    setup and shadcnui addded

---
+++ src/app/components/MaxWidthContainer.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+const MaxWidthContainer = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn("mx-auto px-4 md:px-20 w-full max-w-screen-xl", className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthContainer;

b7b5ce55fc86f0496106e20d4c6712531b3e25bf
Vishal-m <vishalmahesh.study@gmail.com>
2023-12-16 01:01:41 +0530

    uploadthing init

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,37 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const auth = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return null;
+  }
+  return user;
+};
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileCount: 1, maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const user = await auth();
+
+      if (!user) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      return {
+        file,
+        userId: metadata.userId,
+      };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;

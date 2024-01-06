9b1dd168a4910731c62e300bbcb6c0b450a0f25e
Vishal-m <vishalmahesh.study@gmail.com>
2024-01-06 15:19:34 +0530

    ui: pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,176 @@
+import MaxWidthContainer from "@/components/MaxWidthContainer";
+import React from "react";
+import { PLANS } from "@/config/stripe";
+import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Check, Divide, HelpCircle } from "lucide-react";
+
+const pricingItems = [
+  {
+    plan: "Free",
+    tagline: "For small side projects.",
+    quota: 10,
+    features: [
+      {
+        text: "5 pages per PDF",
+        footnote: "The maximum amount of pages per PDF-file.",
+      },
+      {
+        text: "4MB file size limit",
+        footnote: "The maximum file size of a single PDF file.",
+      },
+      {
+        text: "Mobile-friendly interface",
+      },
+      {
+        text: "Higher-quality responses",
+        footnote: "Better algorithmic responses for enhanced content quality",
+        negative: true,
+      },
+      {
+        text: "Priority support",
+        negative: true,
+      },
+    ],
+  },
+  {
+    plan: "Pro",
+    tagline: "For larger projects with higher needs.",
+    quota: PLANS.find((p) => p.slug === "pro")!.quota,
+    features: [
+      {
+        text: "25 pages per PDF",
+        footnote: "The maximum amount of pages per PDF-file.",
+      },
+      {
+        text: "16MB file size limit",
+        footnote: "The maximum file size of a single PDF file.",
+      },
+      {
+        text: "Mobile-friendly interface",
+      },
+      {
+        text: "Higher-quality responses",
+        footnote: "Better algorithmic responses for enhanced content quality",
+      },
+      {
+        text: "Priority support",
+      },
+    ],
+  },
+];
+
+const PricingPage = () => {
+  return (
+    <MaxWidthContainer className="max-w-5xl text-center mt-24 mb-12">
+      <div className="space-y-2">
+        <h1 className="font-bold text-3xl">Pricing</h1>
+        <h3 className="text-slate-500 text-md lg:text-lg">
+          Whether you&apos;re just trying out our service or need more,
+          we&apos;ve got you covered
+        </h3>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-12">
+        {PLANS.map(({ name, slug, quota, pagesPerPdf, price }) => {
+          const planCost = price.amount;
+          const isPro = name.toLocaleLowerCase() === "pro";
+          const features = pricingItems.find(
+            (p) => p.plan.toLowerCase() === name.toLowerCase()
+          )?.features!;
+
+          return (
+            <div
+              className={cn(
+                "relative border border-slate-200 shadow-lg bg-slate-50 rounded-md pb-6 pt-2",
+                {
+                  "border-2 border-primary shadow-red-200": isPro,
+                }
+              )}
+            >
+              <div>
+                <div
+                  className={cn(
+                    "absolute left-0 right-0 -top-3 font-bold text-center mx-auto w-32 py-0.5 rounded-xl text-sm",
+                    {
+                      "bg-gradient-to-r from-primary to-red-700 text-white":
+                        isPro,
+                      "bg-slate-300": !isPro,
+                    }
+                  )}
+                >
+                  {isPro ? "Upgrade now" : "Get started"}
+                </div>
+                <div className="mt-6">
+                  <h3 className=" text-xl font-bold">{name}</h3>
+                  <p className="text-sm text-slate-400">
+                    {isPro
+                      ? "For larger projects with higher needs"
+                      : "For small side projects"}
+                  </p>
+                </div>
+                <div className="text-5xl mt-8 font-bold">
+                  ${price.amount}
+                  <p className="text-lg font-light text-slate-400 my-2">
+                    per month
+                  </p>
+                </div>
+              </div>
+              <div className="w-full bg-slate-100 py-2 border-y">
+                <p className="flex justify-center items-center gap-2">
+                  {isPro ? "50 PDFs/mon included" : "12 PDFs/mon included"}
+                  <TooltipProvider>
+                    <Tooltip delayDuration={300}>
+                      <TooltipContent className="text-xs">
+                        {isPro
+                          ? "Upload upto 50 PDFs a month"
+                          : "Upload upto 12 PDFs a month for free"}
+                      </TooltipContent>
+                      <TooltipTrigger>
+                        <HelpCircle className="h-4 w-4 text-slate-500 cursor-default" />
+                      </TooltipTrigger>
+                    </Tooltip>
+                  </TooltipProvider>
+                </p>
+              </div>
+              <ul className="flex flex-col gap-3  justify-center px-12 pt-8">
+                {features.map(({ text, footnote, negative }) => {
+                  return (
+                    <li className="flex gap-2">
+                    <Check className={cn("h-4 w-4", {
+                        "text-slate-300": negative,
+                        "text-primary shadow-sm rounded-full shadow-primary": !negative
+                    })}/>
+                      {text}
+                      <span>
+                        { !negative && footnote &&
+                          <TooltipProvider>
+                            <Tooltip delayDuration={300}>
+                              <TooltipContent className="text-xs">
+                                {footnote}
+                              </TooltipContent>
+                              <TooltipTrigger>
+                                <HelpCircle className="h-4 w-4 text-slate-500 cursor-default" />
+                              </TooltipTrigger>
+                            </Tooltip>
+                          </TooltipProvider>
+                        }
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </MaxWidthContainer>
+  );
+};
+
+export default PricingPage;
